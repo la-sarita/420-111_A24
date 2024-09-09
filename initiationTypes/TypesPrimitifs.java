1065fa01d1dd90addbc14fd285705696817f751a
Sara Boumehraz <sara.boumehraz@cegepmv.ca>
2024-09-09 11:10:36 -0400

    Suite types primitifs et conversion des types

--- initiationTypes/TypesPrimitifs.java
+++ initiationTypes/TypesPrimitifs.java
@@ -7,7 +7,15 @@
  */
 public class TypesPrimitifs
 {
+    static int unEntier = 25;
+    // Constantes de classe (globale)
+    public static final int AGE_MAX = 120; 
+    public static final double PI = 3.14159; 
+    public static final String MESSAGE_BIENVENUE = "Bienvenue!"; // Constante de type chaîne
+    
     public static void main(String[] args) {
+        // Constante locale 
+        final double TAUX_TVA = 0.20;
         /* * * * * * * * * * *
          * Les types entiers *
          * * * * * * * * * * */
@@ -32,14 +40,42 @@ public class TypesPrimitifs
          * * * * * * * * * * */
         float note = 78.5F; 
         double montant = 586025.50;
+                
+        /* * * * * * * * * * * *
+         *  Le type caractère  *
+         * * * * * * * * * * * */
+        char lettre1 = 'A';
+        char lettre2 = 'a';
+        char chiffre = '3';
+        char symbole = '$';
+        char space = ' ';
+         
+        /* * * * * * * * * * *
+         *  Le type booléen  *
+         * * * * * * * * * * */ 
+        boolean estActive = true;
+        boolean estAbsent = false;
         
+        // Modification de variable
+        age = 20;
+
         System.out.print("L'age est : ");
         System.out.println(age);
-        System.out.println(annee);
-        System.out.println(nbrHabitPays);
-        System.out.println(numTelephone);
-        System.out.println(mois);
-        System.out.println(numDa);
-        System.out.println(montant);
+        System.out.println("L'annéée est : " + annee);
+        System.out.println("Le nombre d'habitants du pays est : " + nbrHabitPays);
+        System.out.println("Le numéro de téléphone est : " + numTelephone);
+        System.out.println("Le mois : " + mois);
+        System.out.println("Numéro de DA" + numDa);
+        System.out.println("Le montant est : " + montant + '$');
+        System.out.println("Une lettre majuscule : " + lettre1);
+        System.out.println("Une lettre miniscule : " + lettre2);
+        System.out.println("Le chiffre en caractère : " + chiffre);
+        System.out.println("Un symbole : " + symbole);
+        System.out.println("Un espace :|" + space + '|');
+        System.out.println(unEntier);
+        System.out.println(AGE_MAX);
+        System.out.println(PI);
+        System.out.println(MESSAGE_BIENVENUE);
+        System.out.println(TAUX_TVA);
     }
 }
