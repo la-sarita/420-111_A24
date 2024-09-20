33e1f3d545a62f7bfa70fee91864e011ff4150f3
la-sarita <sara.boumehraz@collegemv.qc.ca>
2024-09-20 12:16:12 -0400

    Ajout du code de l'exemple validation pseudocode et exo9 du lab2

--- instructionsDeControle/CalculSalaire.java
+++ instructionsDeControle/CalculSalaire.java
@@ -7,57 +7,52 @@
  */
 public class CalculSalaire
 {
-    public static void main(String[] args){
+    public static void main(String[] agrs){
         // Déclaration des constantes 
-        final float TAUX_HORAIRE = 45f;
+        final float TAUX_HORAIRE = 45.0f;
         final float TAUX_HOR_SURPLUS = 1.5f;
-        final float NB_HEURES_REG_MAX = 40f;
-        final float NB_HEURES_MIN = 0f;
-        final float NB_HEURES_MAX = 60f;
+        final float NB_HEURES_REG_MAX = 40.0f;
+        final float NB_HEURES_MIN = 0.0f;
+        final float NB_HEURES_MAX = 60.0f;
         final char PETIT_O = 'o';
-        final char GRAND_O = 'O';
         final char PETIT_N = 'n';
+        final char GRAND_O = 'O';
         final char GRAND_N = 'N';
-        final String MSG_REP_CALC_SALAIRE = "Désirez-vous calculer un salaire?(O ou o = OUI, N ou n = NON) : ";
+        final String MSG_CHOIX_CALC_SALAIRE = "Désirez-vous calculer un salaire?(O ou o = OUI, N ou n = NON) : ";
         final String MSG_ERR_REPONSE = "***Réponse invalide !";
-        final String MSG_SAIS_NBR_HEURES = "Entrez un nombre d’heures de travail: ";
-        final String MSG_ERR_NBRE_HEURES = "***Nombre d’heures non valide";
+        final String MSG_SAISI_NBR_HEURES = "Entrez un nombre d’heures de travail: ";
+        final String MSG_ERR_NBR_HEURES = "***Nombre d’heures non valide";
         
         // Déclaration des variables 
         float nbHeuresSaisi;
-        float nbHeuresReg;
-        float nbHeuresSuppl;
+        float nbHeuresReg ;
+        float nbHeuresSuppl ;
         float salaireReg;
         float salaireSuppl;
         float salaireTotal;
         char reponse;
         
-        do {
-            // Demander à l’utilisateur s’il désire calculer un autre salaire
-            System.out.println(MSG_REP_CALC_SALAIRE);
-            // Lecture de la réponse de l'utilisateur
+        do{
+            System.out.println(MSG_CHOIX_CALC_SALAIRE);
+            // Lire la réponse saisie par l'utilisateur
             reponse = Clavier.lireCharacter();
-            // Validation de la réponse
-            while(reponse != PETIT_O && reponse != GRAND_O 
-                        && reponse != PETIT_N && reponse != GRAND_N){
+            while(reponse != PETIT_O && reponse != GRAND_O && reponse != PETIT_N && reponse != GRAND_N){
                 System.out.println(MSG_ERR_REPONSE);
-                System.out.println(MSG_REP_CALC_SALAIRE);
+                System.out.println(MSG_CHOIX_CALC_SALAIRE);
                 reponse = Clavier.lireCharacter();
             }
             
-            // Calcul du salaire si l'utilisateur avait saisi 'o' ou 'O'
             if(reponse == PETIT_O || reponse == GRAND_O){
-                // Calculer le salaire
-                // Demander à l’utilisateur de saisir le nombre d'heures travaillées
-                System.out.println(MSG_SAIS_NBR_HEURES);
-                // Lecture du nombre d'ehures saisi par l'utilisateur
+                // Calcul du salaire
+                System.out.println(MSG_SAISI_NBR_HEURES);
                 nbHeuresSaisi = Clavier.lireFloat();
+                
                 while(nbHeuresSaisi <= NB_HEURES_MIN || nbHeuresSaisi >= NB_HEURES_MAX){
-                    System.out.println(MSG_ERR_NBRE_HEURES);
-                    System.out.println(MSG_SAIS_NBR_HEURES);
+                    System.out.println(MSG_ERR_NBR_HEURES);
+                    System.out.println(MSG_SAISI_NBR_HEURES);
                     nbHeuresSaisi = Clavier.lireFloat();
                 }
-            
+                
                 // Bloc de calcul ou de traitement
                 if(nbHeuresSaisi <= NB_HEURES_REG_MAX){
                     nbHeuresSuppl = 0;
@@ -65,11 +60,12 @@ public class CalculSalaire
                     nbHeuresReg = nbHeuresSaisi;
                 } else {
                     nbHeuresReg = NB_HEURES_REG_MAX;
-                    nbHeuresSuppl = nbHeuresSaisi - NB_HEURES_REG_MAX;        
+                    nbHeuresSuppl = nbHeuresSaisi - NB_HEURES_REG_MAX;
                     salaireSuppl = nbHeuresSuppl * TAUX_HORAIRE * TAUX_HOR_SURPLUS;
                 }
                 salaireReg = nbHeuresReg * TAUX_HORAIRE;
                 salaireTotal = salaireReg + salaireSuppl;
+                
                 // Bloc d’affichage
                 System.out.println("Le nombre d’heures de travail total: " + nbHeuresSaisi);
                 System.out.println("Le nombre d’heures régulières de travail : " + nbHeuresReg);
