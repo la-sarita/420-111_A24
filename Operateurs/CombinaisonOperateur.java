1d5179c762ec441c822c763f5be4877805c11044
la-sarita <sara.boumehraz@collegemv.qc.ca>
2024-09-28 15:59:57 -0400

    Ajout du code démo opérateur

---
+++ Operateurs/CombinaisonOperateur.java
@@ -0,0 +1,21 @@
+
+/**
+ * Décrivez votre classe CombinaisonOperateur ici.
+ *
+ * @author (votre nom)
+ * @version (un numéro de version ou une date)
+ */
+public class CombinaisonOperateur
+{
+    public static void main(String[] args){
+        int i = 8;
+        int j = 5;
+        float x = 2.0f;
+        double y = 1.0;
+        char c = 'c';
+        char d = 'd';
+        String s = "Bonjour";
+        
+        System.out.println(('A' + 0));
+    }
+}
