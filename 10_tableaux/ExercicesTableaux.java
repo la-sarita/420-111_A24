18867c776d37b7272009f244a8c2ee62081fa607
la-sarita <sara.boumehraz@collegemv.qc.ca>
2024-11-01 03:38:52 -0400

    Ajouter la base de code de l'exo1 sur les tableaux

---
+++ 10_tableaux/ExercicesTableaux.java
@@ -0,0 +1,33 @@
+
+/**
+ * Décrivez votre classe ExercicesTableaux ici.
+ *
+ * @author (votre nom)
+ * @version (un numéro de version ou une date)
+ */
+public class ExercicesTableaux{
+    public static void afficheResult(){
+        System.out.println("#############               ARRAYS             ###############");
+        System.out.println("## ASSIGNATIONS ##");
+        // Créez un tableau nommé noms qui contient les noms de 5 personnes
+
+        // Créez un tableau contenant les tailles de ces 5 personnes en ordre
+
+        System.out.println("## UTILISATIONS ##");
+
+        // Afficher le nom et la taille de la 3e entrée du tableau
+
+        // Afficher la longueur du tableau noms
+
+        System.out.println("## BOUCLES ##");
+        // Afficher le nom et la taille de toutes les personnes dans le tableau noms.
+
+    }
+    
+    // Déclarer une fontion qui prend en argument un tableau de int et renvoi l'avant dernier le plus grand.
+    /**
+     * Renvoi le deuxieme chiffre le plus grand
+     * @param tableau Tableau de chiffre dans lequel on cherche l'avant dernier plus grand
+     * @return un entier represente l'avant dernier le plus grand d'un tableau
+     */
+}
---
+++ 10_tableaux/tableaux.java
@@ -0,0 +1,25 @@
+
+/**
+ * Décrivez votre classe tableaux ici.
+ *
+ * @author (votre nom)
+ * @version (un numéro de version ou une date)
+ */
+public class tableaux{
+    public static void main(String[] args){
+        System.out.println("#############               ARRAYS             ###############");
+        System.out.println("## ASSIGNATIONS ##");
+        // Créez un tableau 
+        
+        System.out.println("## UTILISATIONS ##");
+
+        // Afficher les éléments d'un tableau
+
+        // Afficher la longueur du tableau 
+
+        System.out.println("## BOUCLES FOR ##");
+        
+        System.out.println("## BOUCLES FOREACH ##");
+        
+    }
+}
